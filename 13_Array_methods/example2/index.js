83e45732ee2f52366efeb8df9bf5a1efb518bdc7
Anton Marenholts <ahtoh.ukr@gmail.com>
2025-05-09 09:02:03 +0200

    added to lesson13

--- 13_Array_methods/example2/index.js
+++ 13_Array_methods/example2/index.js
@@ -23,3 +23,47 @@ console.log(drinks);
 // 2
 drinks.forEach((drink) => drink.strength++);
 console.log(drinks);
+
+
+// reverse - мутирующий!!! Но возвращает ссылку на исходный массив
+
+drinks.reverse();
+console.log(drinks);
+
+// sort
+
+const parts = ["air filter", "tires", "spark plugs", "accumulator"];
+parts.sort();
+console.log(parts);
+
+const nums = [1,4,65,23,16,-9,112,21,12];
+nums.sort();
+console.log(nums);// отсортировал как STRING
+
+
+// -1, 0, 1
+nums.sort((a,b) => a-b);// обычная сортировка а - b
+console.log(nums);
+
+nums.sort((a,b) => b - a);// обратная сортировка а - b
+console.log(nums);
+
+parts.sort((a,b) => b.localeCompare(a));
+
+
+drinks.sort((a,b) => a.price - b.price);
+console.log(drinks);
+
+// сравнение по нескольким параметрам
+// сначала сравним по цене
+// если цена совпадает,то сортируем по названию - по алфавиту
+
+drinks.sort((a,b) => {
+    const priceResult = a.price - b.price;
+    if(priceResult === 0){
+        // проверяем по алфавиту
+        return a.name.localeCompare(b.name);
+    }
+    return priceResult;
+});
+console.log(drinks);
