24c18f483ab8eb1c5e9614833755c0a8afd004a4
Anton Marenholts <ahtoh.ukr@gmail.com>
2025-05-12 13:14:15 +0200

    add lesson 15

---
+++ 15_DOM_2/example1/js/index.js
@@ -0,0 +1,15 @@
+// const emailInput = document.getElementById("email");
+
+// emailInput.value = "john@gmail.com"
+// console.log(emailInput.value);
+
+const form = document.getElementById("student-form");
+const emailInput = form.elements["email"];
+
+function onSubmit(event) {
+  event.preventDefault();
+  //   console.log("submit");
+  console.log(emailInput.value);
+}
+
+form.addEventListener("submit", onSubmit);
